4553cd0db04ff252dd659f32b26521186084d39f
Sai Lohitaksh Reddy D <74712527+sailohitaksh-cryptic@users.noreply.github.com>
2023-06-24 10:25:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,39 +3,28 @@ from transformers import pipeline
 
 def summarize_text(article):
     summarizer = pipeline('summarization')
-    max_length = round(len(article.split(' ')) * 0.5)
-    summary = summarizer(article, max_length=max_length, min_length=30, do_sample=False)
+    summary = summarizer(article, max_length=130, min_length=30, do_sample=False)
     return summary[0]['summary_text']
-
-def copy_text(text):
-    st.experimental_set_query_params(copy=text)
-    st.info("Text copied to clipboard")
 
 
 st.title("Text Summarization App")
 
 # Text input field
 user_input = st.text_area("Enter the text to summarize")
-st.write(f"Length of Article: {len(user_input.split(' '))-1}")
 
 if st.button("Summarize"):
     if user_input:
         summary_text = summarize_text(user_input)
 
         # Display summarized text
-        st.write("Summarized Text:")
-        st.write(summary_text)
+        st.subheader("Summarized Text")
+        st.text(summary_text)
 
         # Display length of article and summary
+        st.write(f"Length of Article: {len(user_input.split(' '))}")
         st.write(f"Length of Summary: {len(summary_text.split(' '))}")
 
         # Copy to clipboard
-        copy_text(summary_text)
-
-        # Retrieve copied text from query parameters
-        copied_text = st.experimental_get_query_params().get("copy", "")
-        if copied_text:
-            st.experimental_set_query_params()
-            st.experimental_rerun()
+        st.button("Copy Summarized Text", to_copy=summary_text)
     else:
         st.warning("Please enter some text.")
