31f916193291f8e2baa0c0fe6bbafeda3151b7c7
sailohitaksh-cryptic <74712527+sailohitaksh-cryptic@users.noreply.github.com>
2023-06-24 09:49:21 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from transformers import pipeline
+
+def summarize_text(article):
+    summarizer = pipeline('summarization')
+    summary = summarizer(article, max_length=130, min_length=30, do_sample=False)
+    return summary[0]['summary_text']
+
+
+st.title("Text Summarization App")
+
+# Text input field
+user_input = st.text_area("Enter the text to summarize")
+
+if st.button("Summarize"):
+    if user_input:
+        summary_text = summarize_text(user_input)
+
+        # Display summarized text
+        st.subheader("Summarized Text")
+        st.text(summary_text)
+
+        # Display length of article and summary
+        st.write(f"Length of Article: {len(user_input.split(' '))}")
+        st.write(f"Length of Summary: {len(summary_text.split(' '))}")
+
+        # Copy to clipboard
+        st.button("Copy Summarized Text", to_copy=summary_text)
+    else:
+        st.warning("Please enter some text.")
